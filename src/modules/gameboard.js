ad3d6c83f64f6d751bb23f800f2828e1b32b9968
HasnatSN <hasnat.kontakt@gmail.com>
2022-08-04 20:35:50 +0200

    trying unit tests on classes

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,1 +1,7 @@
- 
+class Gameboard {
+  constructor() {
+    this.size = 10; // 10x10 to make the grid
+  }
+}
+
+module.exports = Gameboard;
--- src/test/gameboard.spec.js
+++ src/test/gameboard.spec.js
@@ -1,7 +1,12 @@
-const gameboard = require("battleships/src/modules/gameboard.js");
+const Gameboard = require("battleships/src/modules/gameboard.js");
 
 describe("test for the gameboard", () => {
-  test("is an object returned?", () => {
-    expect(gameboard()).toEqual({"object"});
+  const gameboard = new Gameboard();
+
+  test("check if the gameboard size is 10", () => {
+    expect(gameboard.size).toEqual(10);
   });
+
+  test("")
+
 });
